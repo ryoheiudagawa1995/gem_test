e651d9564252513d2089301561826b65e0a99191
ryohei udagawa <r.udagawa1995@gmail.com>
2020-06-13 10:24:52 +0900

    add: deviseのgemテスト

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
+  resources :blogs
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+end
